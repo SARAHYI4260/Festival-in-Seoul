560f761f96f11295f1e4ce60b177ce96fcfaef3f
SARAHYI4260 <dltmfrl0608@gmail.com>
2026-07-25 23:12:06 +0900

    feat: 4일차 Spring Boot 백엔드 환경 구축 및 테스트 API, DB 환경 세팅

---
+++ backend/src/main/java/Jar/TestController.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Hello World! 서울 페스티벌 백엔드 서버가 성공적으로 열렸습니다!";
+    }
+}
